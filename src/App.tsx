6649c5d665e57ac7045414f7f1d9e0c39d5ac08d
Killian KOPP <killiankopp@gmail.com>
2025-06-19 21:35:15 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello World! This is a simple React application.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
