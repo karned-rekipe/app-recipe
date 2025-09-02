6b05f7bd1785bb509509a99c171f2e4be3490edf
Killian KOPP <killiankopp@gmail.com>
2025-09-02 23:13:57 +0200

    Add PWA support with manifest and service worker
    - Added a manifest.json file for the PWA configuration, including app name, description, icons, and theme colors.
    - Implemented a service worker (service-worker.js) to cache essential resources for offline access and improve loading performance.
    - Included a 512x512 icon (icon-512.png) for the application.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,24 +2,47 @@ import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { AuthProvider } from '../contexts/AuthContext';
 import { AuthGuard } from '../components/auth/AuthGuard';
+import React, { useEffect } from 'react';
+import Head from 'expo-router/head';
 
 export default function RootLayout() {
+  useEffect(() => {
+    if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/service-worker.js')
+        .then((registration) => {
+          console.log('SW registered: ', registration);
+        })
+        .catch((registrationError) => {
+          console.log('SW registration failed: ', registrationError);
+        });
+    }
+  }, []);
+
   return (
-    <AuthProvider>
-      <AuthGuard>
-        <Stack>
-          <Stack.Screen name="login" options={{ headerShown: false }} />
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen 
-            name="recipe-details" 
-            options={{ 
-              headerShown: false,
-              presentation: 'modal' // Pour une transition plus fluide
-            }} 
-          />
-        </Stack>
-        <StatusBar style="light" />
-      </AuthGuard>
-    </AuthProvider>
+    <>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#ffb300" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Rekipe" />
+      </Head>
+      <AuthProvider>
+        <AuthGuard>
+          <Stack>
+            <Stack.Screen name="login" options={{ headerShown: false }} />
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen 
+              name="recipe-details" 
+              options={{ 
+                headerShown: false,
+                presentation: 'modal' // Pour une transition plus fluide
+              }} 
+            />
+          </Stack>
+          <StatusBar style="light" />
+        </AuthGuard>
+      </AuthProvider>
+    </>
   );
 }
